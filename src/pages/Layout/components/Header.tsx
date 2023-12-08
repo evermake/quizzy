97b7419f7d26f25a47027238aaff2f5dd23714d1
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2023-12-08 10:27:21 +0300

    refactor: remove logic of user

--- src/pages/Layout/components/Header.tsx
+++ src/pages/Layout/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useGetUserQuery } from '@/store/services/userService'
 import { AppRoute } from '@/constants'
 
 function Header() {
